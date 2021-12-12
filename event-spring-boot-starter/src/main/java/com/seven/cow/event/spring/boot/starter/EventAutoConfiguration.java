ef4f13c741d035e2e8f2b6a06e230221858fc423
wangyongyong <yongyongwang1993@gmail.com>
2021-12-12 12:46:03 +0800

    version:0.0.3

--- event-spring-boot-starter/src/main/java/com/seven/cow/event/spring/boot/starter/EventAutoConfiguration.java
+++ event-spring-boot-starter/src/main/java/com/seven/cow/event/spring/boot/starter/EventAutoConfiguration.java
@@ -5,15 +5,13 @@ import com.seven.cow.event.spring.boot.starter.service.EventService;
 import com.seven.cow.event.spring.boot.starter.service.impl.BusinessEventThreadFactory;
 import com.seven.cow.event.spring.boot.starter.service.impl.EventServiceCombination;
 import com.seven.cow.event.spring.boot.starter.service.impl.EventServiceImpl;
+import com.seven.cow.spring.boot.autoconfigure.util.LoggerUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Configuration
 @EnableConfigurationProperties(EventExecutorProperties.class)
@@ -21,20 +19,29 @@ public class EventAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public EventService eventService(){
+    public EventService eventService() {
         return new EventServiceImpl();
     }
 
     @Bean
     @ConditionalOnMissingBean
-    public EventServiceCombination eventServiceCombination(){
+    public EventServiceCombination eventServiceCombination() {
         return new EventServiceCombination();
     }
 
     @Bean
     @ConditionalOnMissingBean
     public ExecutorService businessEventExecutorService(EventExecutorProperties executorProperties) {
-        return new ThreadPoolExecutor(executorProperties.getCorePoolSize(), executorProperties.getMaximumPoolSize(), executorProperties.getKeepAliveTime(), TimeUnit.SECONDS, new SynchronousQueue<>(), new BusinessEventThreadFactory(executorProperties.getNamePrefix()), new ThreadPoolExecutor.CallerRunsPolicy());
+        RejectedExecutionHandler rejectedExecutionHandler = new ThreadPoolExecutor.CallerRunsPolicy();
+        Class<RejectedExecutionHandler> rejectedExecutionHandlerClass = executorProperties.getRejectedExecutionHandler();
+        if (null != rejectedExecutionHandlerClass) {
+            try {
+                rejectedExecutionHandler = rejectedExecutionHandlerClass.newInstance();
+            } catch (InstantiationException | IllegalAccessException e) {
+                LoggerUtils.error(e.getMessage(), e);
+            }
+        }
+        return new ThreadPoolExecutor(executorProperties.getCorePoolSize(), executorProperties.getMaximumPoolSize(), executorProperties.getKeepAliveTime(), TimeUnit.SECONDS, new SynchronousQueue<>(), new BusinessEventThreadFactory(executorProperties.getNamePrefix()), rejectedExecutionHandler);
     }
 
 }
--- event-spring-boot-starter/src/main/java/com/seven/cow/event/spring/boot/starter/properties/EventExecutorProperties.java
+++ event-spring-boot-starter/src/main/java/com/seven/cow/event/spring/boot/starter/properties/EventExecutorProperties.java
@@ -2,54 +2,73 @@ package com.seven.cow.event.spring.boot.starter.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.concurrent.RejectedExecutionHandler;
+
 @ConfigurationProperties(prefix = "event.executor.pool")
-public class EventExecutorProperties
-{
+public class EventExecutorProperties {
+
+    /**
+     * 核心保留线程数
+     */
     private Integer corePoolSize = 10;
 
+    /*
+     * 最大线程数
+     */
     private Integer maximumPoolSize = 50;
 
+    /**
+     * 线程池中线程存活时间
+     */
     private Long keepAliveTime = 60L;
 
+    /**
+     * 线程池名称前缀
+     */
     private String namePrefix = "business-event-";
 
-    public Integer getCorePoolSize()
-    {
+    /**
+     * 线程池拒绝处理类
+     */
+    private Class<RejectedExecutionHandler> rejectedExecutionHandler;
+
+    public Integer getCorePoolSize() {
         return corePoolSize;
     }
 
-    public void setCorePoolSize(Integer corePoolSize)
-    {
+    public void setCorePoolSize(Integer corePoolSize) {
         this.corePoolSize = corePoolSize;
     }
 
-    public Integer getMaximumPoolSize()
-    {
+    public Integer getMaximumPoolSize() {
         return maximumPoolSize;
     }
 
-    public void setMaximumPoolSize(Integer maximumPoolSize)
-    {
+    public void setMaximumPoolSize(Integer maximumPoolSize) {
         this.maximumPoolSize = maximumPoolSize;
     }
 
-    public Long getKeepAliveTime()
-    {
+    public Long getKeepAliveTime() {
         return keepAliveTime;
     }
 
-    public void setKeepAliveTime(Long keepAliveTime)
-    {
+    public void setKeepAliveTime(Long keepAliveTime) {
         this.keepAliveTime = keepAliveTime;
     }
 
-    public String getNamePrefix()
-    {
+    public String getNamePrefix() {
         return namePrefix;
     }
 
-    public void setNamePrefix(String namePrefix)
-    {
+    public void setNamePrefix(String namePrefix) {
         this.namePrefix = namePrefix;
+    }
+
+    public Class<RejectedExecutionHandler> getRejectedExecutionHandler() {
+        return rejectedExecutionHandler;
+    }
+
+    public void setRejectedExecutionHandler(Class<RejectedExecutionHandler> rejectedExecutionHandler) {
+        this.rejectedExecutionHandler = rejectedExecutionHandler;
     }
 }
