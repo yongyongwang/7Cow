d18facb9e7d2c035f400be4e68bd21247681a29e
wangyongyong <wangyongyong@zj.tech>
2022-04-13 10:42:49 +0800

    7Cow Cache

--- servlet-cache/src/main/java/com/seven/cow/servlet/cache/service/impl/DefaultCacheStorageManagerImpl.java
+++ servlet-cache/src/main/java/com/seven/cow/servlet/cache/service/impl/DefaultCacheStorageManagerImpl.java
@@ -1,5 +1,7 @@
 package com.seven.cow.servlet.cache.service.impl;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.seven.cow.servlet.cache.service.CacheStorageManager;
 
 import java.util.concurrent.TimeUnit;
@@ -11,23 +13,36 @@ import java.util.concurrent.TimeUnit;
  * @version: 1.0
  */
 public class DefaultCacheStorageManagerImpl implements CacheStorageManager {
+
+    private static final Cache<String, Object> cacheManager = Caffeine.newBuilder().build();
+
     @Override
     public void set(String key, Object value) {
-
+        cacheManager.put(key, value);
     }
 
     @Override
     public void set(String key, Object value, TimeUnit timeUnit, long timeout) {
-
+        Cache<String, Object> cache = Caffeine.newBuilder()
+                .expireAfterWrite(timeout, timeUnit)
+                .maximumSize(1L)
+                .initialCapacity(1)
+                .build();
+        cache.put(key, value);
+        cacheManager.put(key, cache);
     }
 
     @Override
     public Object get(String key) {
-        return null;
+        Object object = cacheManager.getIfPresent(key);
+        if (object instanceof Cache) {
+            return ((Cache) object).getIfPresent(key);
+        }
+        return object;
     }
 
     @Override
     public void remove(String key) {
-
+        cacheManager.invalidate(key);
     }
 }
