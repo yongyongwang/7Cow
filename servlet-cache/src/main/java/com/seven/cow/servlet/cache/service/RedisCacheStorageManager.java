5a6372f5eb3d464fed17319e9e0ab79d20fef16b
wangyongyong <wangyongyong@zj.tech>
2022-04-14 16:26:48 +0800

    7Cow Cache

--- servlet-cache/src/main/java/com/seven/cow/servlet/cache/service/RedisCacheStorageManager.java
+++ servlet-cache/src/main/java/com/seven/cow/servlet/cache/service/RedisCacheStorageManager.java
@@ -22,9 +22,31 @@ public interface RedisCacheStorageManager extends CacheStorageManager {
     Boolean persist(String key);
 
 
+    /**
+     * 返回key的剩余的过期时间
+     *
+     * @param key      键
+     * @param timeUnit 时间单位
+     * @return 过期时间
+     */
     Long getExpire(String key, TimeUnit timeUnit);
 
+    /**
+     * 修改key的名称
+     *
+     * @param oldKey 要修改的键值
+     * @param newKey 修改后的键值
+     */
     void rename(String oldKey, String newKey);
+
+    /**
+     * 将给定 key 的值设为 value ，并返回key的旧值
+     *
+     * @param key   键
+     * @param value 值
+     * @return key的旧值
+     */
+    Object getAndSet(String key, Object value);
 
 
 }
